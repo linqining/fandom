d8c631612fc2346e4cf719a2f839c55b1afc1d0c
linqining <linqining@outlook.com>
2025-05-29 09:22:45 +0800

    feat:pump

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import "@radix-ui/themes/styles.css";
 
 import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Theme } from "@radix-ui/themes";
 import App from "./App.tsx";
 import { networkConfig } from "./networkConfig.ts";
 
@@ -13,7 +12,6 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Theme appearance="dark">
       <QueryClientProvider client={queryClient}>
         <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
           <WalletProvider autoConnect>
@@ -21,6 +19,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           </WalletProvider>
         </SuiClientProvider>
       </QueryClientProvider>
-    </Theme>
   </React.StrictMode>,
 );
